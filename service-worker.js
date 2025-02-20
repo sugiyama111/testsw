780798ce7bec973bd5d6acc7ef02cc8f7daa96f7
sugiyama <sugiyama@neo-system.jp>
2025-02-20 23:06:17 +0900

    ・

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,24 @@
 console.log('sw');
+
+
+let port = null;
+
+self.addEventListener('message', (event) => {
+
+	if (event.data.type === 'azukeru') {
+		console.log('@sw receive azukeru');
+		
+		// ページ側から port を受け取る
+		port = event.data.port;
+	}
+	else if (event.data.type === 'toridasu') {
+		console.log('@sw received toridasu');
+		
+		// ページ側から scannerConnection のリクエストが来たら返す
+		console.log('@sw send port:'+port);
+		if (port && event.source) {
+		    event.source.postMessage({ type: 'kaesu', port }, [port]);
+		}
+	}
+});
+
